8e53c87cedcc7c1bb17043dd1b4e5897489eee96
Rafael Barbosa <rafha4061@gmail.com>
2025-11-20 12:33:17 -0300

    Criação das classes

---
+++ CourseEFCore/Domain/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace CourseEFCore.Domain
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Cidade { get; set; }
+    }
+}
---
+++ CourseEFCore/ValueObjects/StatusPedido.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseEFCore.ValueObjects
+{
+    public enum StatusPedido
+    {
+        Analise,
+        Finalizado,
+        Entregue,
+    }
+}
