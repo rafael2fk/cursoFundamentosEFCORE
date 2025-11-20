8a9b4b4d1717a407f41d890d15eed02fa16f5eaf
Rafael Barbosa <rafha4061@gmail.com>
2025-11-20 13:23:43 -0300

    Utilizando DataAnnotations

--- CourseEFCore/Domain/Cliente.cs
+++ CourseEFCore/Domain/Cliente.cs
@@ -1,11 +1,18 @@
-﻿namespace CourseEFCore.Domain
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseEFCore.Domain
 {
+    [Table("Clientes")] // na bd nao e "cliente" e sim "Clientess"
     public class Cliente
     {
+        [Key]  //chave primaria
         public int Id { get; set; }
 
+        [Required] // propriedade obrigatorio
         public string Nome { get; set; }
 
+        [Column("Phone")]   // informa que pode ter outro nome na tabela "parecido"
         public string Telefone { get; set; }
 
         public string CEP { get; set; }
