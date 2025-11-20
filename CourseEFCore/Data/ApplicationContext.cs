aedaeb5416b2c4c29642182048f5780040883072
Rafael Barbosa <rafha4061@gmail.com>
2025-11-20 13:16:16 -0300

    Arquivos de configurações

--- CourseEFCore/Data/ApplicationContext.cs
+++ CourseEFCore/Data/ApplicationContext.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CourseEFCore.Data.Configurations;
+using CourseEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<Pedido> Pedidos { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CourseEFCore;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // chamando todas as config
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
         }
     }
 }
